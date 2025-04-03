3331668a15226dc17647bfb967748adb936d5f5c
ledemetrio <160793915+ledemetrio@users.noreply.github.com>
2025-04-03 19:29:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessbilidade=document.getElementById('opcoes-acessibilidade');
+    
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessbilidade.classList.toggle('apresenta-lista');
+    
+    const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')=== 'true';
+    
+    botaoDeAcessibilidade.setAttribute('aria-expanded',!botaoSelecionado)
+    
+    }) 
+    
+    
+   const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+   const diminuiFonteBotao = document.getElementById('diminuir-fonte'); 
+
+   const alternaConstraste = document.getElementById('alterna-contraste');
+})
+
